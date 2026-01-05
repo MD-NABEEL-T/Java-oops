d3521b2f07fad17ee5e875bf05488566cdbb90ec
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-05 22:27:11 +0530

    alright updated buddy

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String []args){
+        String a="iron";
+        String b="iron";
+        String c=new String("iron");
+        String d=new String("iron");
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
