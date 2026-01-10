81c994ce5abe5c0e1ce91d08e9181e55b584f2dd
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-10 13:27:08 +0530

    updated

---
+++ FinalKeyword.java
@@ -0,0 +1,33 @@
+final means no modification allowed.
+
+1️⃣ final variable
+👉 Value cannot be changed
+final int x = 10;
+x = 20; // ❌ ERROR
+
+2️⃣ final method
+👉 Method cannot be overridden
+class A {
+    final void show() {
+        System.out.println("Hello");
+    }
+}
+class B extends A {
+    // void show() { }  // ❌ ERROR
+}
+
+
+3️⃣ final class
+👉 Class cannot be inherited
+final class A {
+}
+
+// class B extends A { }  // ❌ ERROR
+
+
+Very common INTERVIEW Q 🔥
+❓ Can a final variable be empty?
+👉 Yes, but only once
+final int x;
+x = 10; // allowed
+x = 20; // ❌ not allowed
---
+++ StaticKeyword.java
@@ -0,0 +1,31 @@
+// one way
+// public class StaticKeyword{
+//     String name="Nabeel";
+//     public static void main(String args[]){
+//         StaticKeyword s=new StaticKeyword();
+//         System.out.println(s.name);
+//     }
+// }
+
+// Static keyword used to indicate that a member belongs to the class itself rather than the instance of a class(objects)
+// second way
+// public class StaticKeyword{
+//     static String name="Nabeel";
+//     public static void main(String []args){
+//         System.out.println(name);
+//     }
+// }
+// the above executes because both are static
+
+//Another example of static
+// public class StaticKeyword{
+//     static{
+//         System.out.println("First ");
+//     }
+//     public static void main(String []args){
+//         System.out.println("second ");
+//     }
+// }
+
+
+// Refer Static1.png and Static2.png 
