499d43c81c41fd0fd7113e8ac70a92e52fca57f2
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-14 09:09:56 +0530

    updated

--- medium/ExceptionHandling.java
+++ medium/ExceptionHandling.java
@@ -1,3 +1,21 @@
-public class ExceptionHandling {
-    
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class ExceptionHandling{
+    public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        try{
+            int a=s.nextInt();
+            System.out.println(10/a);
+        }
+        catch(InputMismatchException e){
+            System.out.println("wrong type");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+        catch(Exception e){
+            System.out.println("Error");
+        }
+        s.close();
+    }
 }
